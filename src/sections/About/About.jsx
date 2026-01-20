4e4d62f0de22898c580910617f604ac2f450fb38
v1rul3nce <teegan.springer@gmail.com>
2026-01-20 16:35:00 -0600

    Basic Social Media Links and Flawed Implementation of About svg

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,3 +1,14 @@
+import "./About.css";
+
 export default function About() {
-  return;
+  return (
+    <section className="about1">
+      <div className="scroll-wrapper">
+        <div className="about-content">
+          <h2>About</h2>
+          <p>This is the About segment sample text.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/sections/SocialMedia/SocialMedia.jsx
+++ src/sections/SocialMedia/SocialMedia.jsx
@@ -1,3 +1,20 @@
+import "./SocialMedia.css";
+import { FaInstagram} from "react-icons/fa6";
+
 export default function SocialMedia() {
-  return;
+  return (
+    <section className="socials">
+      <h3>Follow Us</h3>
+
+      <ul className="social-links">
+        <li>
+          <a href="https://www.instagram.com/wicysatuic" target="_blank">
+            <FaInstagram />
+            <span>Instagram</span>
+          </a>
+        </li>
+        add linkedin, discord server
+      </ul>
+    </section>
+  );
 }
