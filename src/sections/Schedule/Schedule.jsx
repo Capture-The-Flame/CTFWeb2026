ad53ee45b63fd12868bfbc0f4fba1a08bd635731
dyl4915 <dnguy108@BOOK-FA6GTJL991.localdomain>
2026-02-10 16:40:59 -0600

    added sprites and scaling, also changed animations for hover

--- src/sections/Schedule/Schedule.jsx
+++ src/sections/Schedule/Schedule.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import "./Schedule.css";
-import placeholderPerson from "../../assets/scheduleArt/placeholderperson.png";
+import char1 from "../../assets/scheduleArt/character_01.svg";
+import char2 from "../../assets/scheduleArt/character_02.svg";
+import char3 from "../../assets/scheduleArt/character_03.svg";
+import char4 from "../../assets/scheduleArt/character_04.svg";
 
-const scheduleCharacters = [
-  // positioning
-  { id: 1, label: "schedule", top: "20%", left: "10%" },
-  { id: 2, label: "schedule", top: "30%", left: "70%" },
-  { id: 3, label: "schedule",  top: "55%", left: "20%" },
-  { id: 4, label: "schedule",  top: "75%", left: "70%" },
+const scheduleCharacters = [ //properties for each character
+  { 
+    id: 1, 
+    label: "character one", 
+    src: char1, 
+    top: "43%", 
+    left: "35%", 
+    width: "40%", 
+    rotate: "90deg" //pics in file are rotated so had to adjust
+  },
+  { 
+    id: 2, 
+    label: "character two", 
+    src: char2, 
+    top: "70%", 
+    left: "78%", 
+    width: "15%" 
+  },
+  { 
+    id: 3, 
+    label: "character three", 
+    src: char3, 
+    top: "65%", 
+    left: "60%", 
+    width: "17%" 
+  },
+  {
+    id: 4, 
+    label: "character four", 
+    src: char4, 
+    top: "34%", 
+    left: "10%", 
+    width: "30%", 
+    rotate: "90deg" //pics in file are rotated so had to adjust
+  },
 ];
 
 export default function Schedule() {
@@ -17,14 +49,19 @@ export default function Schedule() {
         <button 
           key={char.id} 
           className="char-btn"
-          style={{ top: char.top, left: char.left }}
+          style={{
+                  top: char.top,
+                  left: char.left,
+                  width: char.width
+                }}
           aria-label={`View schedule for ${char.label}`}
           onClick={() => console.log("Open popup for " + char.label)} // schedule pop up for later
         >
           <img 
-            src={placeholderPerson} 
-            alt="Character" 
+            src={char.src} 
+            alt="char.label" 
             className="char-img" 
+            style={{ transform: `rotate(${char.rotate || '0deg'})` }}
           />
           {/* show label on hover */}
           <span className="char-label">{char.label}</span>
