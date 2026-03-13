b39018a5999fb762f5c5dc6ccda348c48a25d9d2
v1rul3nce <teegan.springer@gmail.com>
2026-03-13 17:51:29 -0500

    About message update (revised old about)

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className="about">
       <h2 className="about-title">About</h2>
       <p>
-        I'm putting text here for the About section to see what it looks like if the About section is long enough to wrap around. There will be more text here later.
+        Get ready for another citywide Capture The Flag (CTF) competition happening April 11, 2026 at UIC! Open to students across the city, this in-person event is designed to challenge and inspire cybersecurity enthusiasts of all skill levels. Participants can compete in beginner and advanced tracks, enjoy food and refreshments, and take advantage of professional growth opportunities through workshops, a networking fair, and an insightful keynote from a top cybersecurity expert.
       </p>
     </section>
   );
