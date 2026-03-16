6c10e487a5e646a3370319a1a22c4202a7fcbd60
v1rul3nce <teegan.springer@gmail.com>
2026-03-16 15:25:00 -0500

    about update

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className="about">
       <h2 className="about-title">About</h2>
       <p>
-        Get ready for another citywide Capture The Flag (CTF) competition happening April 11, 2026 at UIC! Open to students across the city, this in-person event is designed to challenge and inspire cybersecurity enthusiasts of all skill levels. Participants can compete in beginner and advanced tracks, enjoy food and refreshments, and take advantage of professional growth opportunities through workshops, a networking fair, and an insightful keynote from a top cybersecurity expert.
+        Get ready for another Capture The Flag (CTF) competition happening April 11, 2026 at UIC! Open to students across the city, this in-person event is designed to challenge and inspire cybersecurity enthusiasts of all skill levels. Participants can compete in beginner and advanced tracks, enjoy food and refreshments, and take advantage of professional growth opportunities through workshops, networking, and an insightful keynote from a top cybersecurity expert.
       </p>
     </section>
   );
